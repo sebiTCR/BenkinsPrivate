9c08cfc2329c1852f6f8b00d3dcebe2169eb50c5
SebiTCR <Ssebi832@outlook.com>
2026-03-27 22:13:06 +0200

    Converted models to Dataclasses

--- persistance/models.py
+++ persistance/models.py
@@ -1,7 +1,9 @@
 from tokenize import String
+from typing import List
+from dataclasses import dataclass
 
-from sqlalchemy import (String, Enum)
-from sqlalchemy.orm import DeclarativeBase, Mapped
+from sqlalchemy import (String, Enum, ForeignKey,)
+from sqlalchemy.orm import DeclarativeBase, Mapped, relationship, MappedAsDataclass
 from sqlalchemy.testing.schema import mapped_column
 
 
@@ -17,14 +19,15 @@ class Base(DeclarativeBase):
     pass
 
 
-class Project(Base):
+class Project(MappedAsDataclass, Base):
         __tablename__ = "project"
 
-        id: Mapped[int] = mapped_column(primary_key=True)
+        builds: Mapped[List["Build"]] = relationship(init=False)
+        id: Mapped[int] = mapped_column(init=False, primary_key=True)
         name: Mapped[str] = mapped_column(String(30))
         version: Mapped[str] = mapped_column(String(10))
-        # status: Mapped[Enum] = mapped_column(Enum("NONE", "SUCESS", "BUILDING", "FAILED", name="status_enum", create_type=True))
         status: Mapped[int] = mapped_column()
+        path: Mapped[str] = mapped_column()
 
 
 class Build(MappedAsDataclass, Base):
