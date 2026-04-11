920d1003260000b5c7d286c7cd3eb0334bf282dd
SebiTCR <Ssebi832@outlook.com>
2026-04-11 18:39:50 +0300

    Added warn log

--- core/log.py
+++ core/log.py
@@ -9,8 +9,12 @@ def info(message):
     print(f'{Fore.grey_100}{Back.blue_1} INFO {Style.reset} {message} ')
 
 
+def warn(message):
+    print(f'{Fore.grey_15}{Back.light_yellow} INFO {Style.reset} {message} ')
+
+
 def error(message):
-    print(f'{Fore.grey_15}{Back.red_3a} ERROR {Style.reset} {message} ')
+    print(f'{Fore.grey_100}{Back.red_3a} ERROR {Style.reset} {message} ')
 
 
 def fatal(message):
